168c61bd64c2bb7ed1db4223cebf7dcf6c45904b
w1702 <w1702@outlook.com>
2025-06-04 15:29:32 +1000

    add remaining project files for setup

---
+++ src/main/java/com/example/eatclub/EatclubApplication.java
@@ -0,0 +1,13 @@
+package com.example.eatclub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EatclubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EatclubApplication.class, args);
+	}
+
+}
